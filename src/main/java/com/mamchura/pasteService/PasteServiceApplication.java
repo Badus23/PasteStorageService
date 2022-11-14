3a3317cc388a1d16031339559e6248b30359b8c8
Bohdan Mamchura <mazdeyf@gmail.com>
2022-11-14 19:34:33 +0100

    init

---
+++ src/main/java/com/mamchura/pasteService/PasteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mamchura.pasteService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamchura/pasteService/PasteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamchura.pasteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
